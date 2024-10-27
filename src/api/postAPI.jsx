2e6e622ac8dcd2d58e6244f0454019da577bd6f1
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-10-27 21:37:04 +0530

    added API to .env file

--- src/api/postAPI.jsx
+++ src/api/postAPI.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 
+// Access the API key using Vite's import.meta.env
+const apiKey = import.meta.env.VITE_API_KEY;
+
+// Create an Axios instance with the base URL
 const api = axios.create({
-  baseURL: "https://v6.exchangerate-api.com/v6/fa1341044d823ba48dc534cc",
+  baseURL: `https://v6.exchangerate-api.com/v6/${apiKey}`,
 });
 
-//we need to create get request
+// Function to convert currencies
 export const currencyConverter = (fromCurrency, toCurrency, amount) => {
   return api.get(`/pair/${fromCurrency}/${toCurrency}/${amount}`);
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "replace-process-env",
+      // Use the "transform" hook to replace process.env variables
+      transform(code) {
+        return code.replace(
+          /process\.env\.VITE_API_KEY/g,
+          JSON.stringify(process.env.VITE_API_KEY)
+        );
+      },
+    },
+  ],
+});
